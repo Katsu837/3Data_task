e659d040c3b6edb0f25c876e1ba44b70799de68c
Katsu837 <mp.200402@gmail.com>
2024-06-11 12:43:42 +0300

    src structure #5

---
+++ src/api/fetcher.js
@@ -0,0 +1,61 @@
+export const fetcherMe = async (url) => {
+  const token = { token: localStorage.getItem("jwtToken") };
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(token),
+    });
+
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const jsonResponse = response.json();
+    jsonResponse
+      .then((result) => {
+        console.log(result.status);
+        if (result.status === "auth") {
+          localStorage.removeItem("jwtToken");
+        }
+      })
+      .catch((e) => console.error(e));
+    console.log(jsonResponse);
+    return jsonResponse;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const fetcherMeEdit = async (url, values) => {
+  const token = localStorage.getItem("jwtToken");
+  const data = { ...values };
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ data, token }),
+    });
+
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+    return response.json();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const authFetcher = async (linkToAuth, values) => {
+  try {
+    const response = await fetch(linkToAuth, {
+      method: "POST",
+      body: JSON.stringify(values),
+    });
+
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const jsonResponse = response.json();
+    jsonResponse
+      .then((result) => {
+        localStorage.setItem("jwtToken", result.data.value);
+      })
+      .catch((e) => console.error(e));
+    return jsonResponse;
+  } catch (e) {
+    console.error(e);
+  }
+};
