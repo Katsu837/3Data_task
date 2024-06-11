a279ec0380c592a39e4056b45a7100217fdf6f3f
Katsu837 <mp.200402@gmail.com>
2024-06-11 12:36:44 +0300

    domens file #8

---
+++ src/utils/domens.js
@@ -0,0 +1,4 @@
+export const dataDomen = "cp.3data.ru";
+export const linkToAuth = `https://${dataDomen}/openapi/auth2`;
+export const linkToMe = `https://${dataDomen}/openapi/me`;
+export const linkToUpdateMe = `https://${dataDomen}/openapi/me/edit`;
