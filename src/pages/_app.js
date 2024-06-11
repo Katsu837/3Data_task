132f84c9f1e7a59d8a487f3aa062824d23c5b7a0
Katsu837 <mp.200402@gmail.com>
2024-06-11 12:27:27 +0300

    new theme logic #1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,46 +1,49 @@
-import {Box, CssBaseline, Switch, ThemeProvider} from "@mui/material";
-import {createTheme} from '@mui/material/styles'
-import {theme as myTheme} from '@/UI/theme'
-import {useLayoutEffect, useState} from "react";
-import {useRouter} from "next/router";
+import { Box, CssBaseline, Switch, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useLayoutEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { theme as myTheme } from "@/utils/theme";
 
+export default function App() {
+  const [mode, setMode] = useState("light");
+  const [showChild, setShowChild] = useState(false);
+  const theme = createTheme(myTheme(mode));
+  const { push, pathname } = useRouter();
 
-export default function App({ Component, pageProps }) {
+  const changeMode = () => {
+    setMode((prevState) => (prevState === "light" ? "dark" : "light"));
+  };
 
-    const [mode, setMode] = useState('light')
-    const [showChild, setShowChild] = useState(false)
-    const theme = createTheme(myTheme(mode))
-    const {push, pathname} = useRouter()
+  const changeThemeMode = () => {
+    const theme = localStorage.getItem("themeMode");
+    changeMode();
+    localStorage.setItem("themeMode", theme === "light" ? "dark" : "light");
+  };
 
+  useLayoutEffect(() => {
+    const themeMode = localStorage.getItem("themeMode");
+    if (themeMode === null) localStorage.setItem("themeMode", "light");
+    else if (mode !== themeMode) changeMode();
 
-    const changeMode = () => {
-        setMode(prevState => (prevState === 'light' ? 'dark' : 'light'
-    ))
-    }
+    const token = localStorage.getItem("jwtToken");
+    if (token === null && pathname !== "/") push("/");
+    else if (token != null && pathname === "/") push("/me");
+    else setShowChild(true);
+  }, [pathname, push, mode]);
 
-    useLayoutEffect(() => {
-        const themeMode = localStorage.getItem('themeMode')
-        if(themeMode === null ) localStorage.setItem('themeMode', 'light')
-        localStorage.setItem('themeMode', mode)
+  console.log(mode);
 
+  if (!showChild) return null;
 
-        const token = localStorage.getItem('jwtToken')
-        if(token === null && pathname !== '/') push('/')
-        else if(token != null && pathname === '/') push('/me')
-        else setShowChild(true)
-    }, [pathname, push, mode])
-
-
-    if (!showChild) return null;
-
-
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Box>
-                <Switch onChange={changeMode} checked={mode !== 'light'}>{mode}</Switch>
-                <Component {...pageProps}/>
-            </Box>
-        </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        <Switch onChange={changeThemeMode} checked={mode !== "light"}>
+          {mode}
+        </Switch>
+        <Component {...pageProps} />
+      </Box>
+    </ThemeProvider>
   );
 }
