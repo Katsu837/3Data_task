f89a97a4fe7b9a7d989652dcf048c9e9b69d807f
Katsu837 <mp.200402@gmail.com>
2024-06-21 17:35:52 +0300

    chore/fix-eslint-in-MeTemplate

--- src/templates/MeTemplate/index.jsx
+++ src/templates/MeTemplate/index.jsx
@@ -2,28 +2,29 @@ import React from "react";
 import { Paper, Stack } from "@mui/material";
 import FormSkeleton from "../../components/FormSkeleton";
 import MeForm from "../../components/MeForm";
-import { useRouter } from "next/router";
 
 function MeTemplate({ data, isLoading, mutate }) {
-  const { push } = useRouter();
+  if (isLoading) {
+    return (
+      <Stack variant="fullPage">
+        <Paper>
+          <FormSkeleton />
+        </Paper>
+      </Stack>
+    );
+  }
 
-  return (
-    <>
-      {isLoading ? (
-        <Stack variant="fullPage">
-          <Paper>
-            <FormSkeleton />
-          </Paper>
-        </Stack>
-      ) : data.status !== "auth" ? (
-        <Stack variant="fullPage">
-          <Paper>
-            <MeForm data={data} mutate={mutate} />
-          </Paper>
-        </Stack>
-      ) : null}
-    </>
-  );
+  if (data.status !== "auth") {
+    return (
+      <Stack variant="fullPage">
+        <Paper>
+          <MeForm data={data} mutate={mutate} />
+        </Paper>
+      </Stack>
+    );
+  }
+
+  return null;
 }
 
 export default MeTemplate;
